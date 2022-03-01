6de6111e2de61e3364b99d788015aa62e7b14d82
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-01 12:37:42 +0900

    const、letの変数宣言

---
+++ src/index.js
@@ -0,0 +1,54 @@
+/**
+ * const、letの変数宣言
+ */
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言";
+// console.log(val1);
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// val3 = "const変数は上書き不可";
+
+// const val3 = "const変数を再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//     name: 'じゃけえ',
+//     age: 28
+// };
+// console.log(val4);
+
+// val4.name = "jake";
+// val4.address = "広島";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+const val5 = ['dog', 'cat'];
+console.log(val5);
+
+val5[0] = 'bird';
+console.log(val5);
+
+val5.push("monkey");
+console.log(val5);
+
