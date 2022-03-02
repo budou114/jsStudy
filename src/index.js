6ba3ef4d13e8a61f2ced10f764ded5b1f2e35381
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-02 12:13:34 +0900

    コメントアウト

--- src/index.js
+++ src/index.js
@@ -147,12 +147,12 @@
 // console.log(arr3);
 
 // 配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
-const arr6 = [...arr4];
-arr6[0] = 100;
-console.log(arr4);
-console.log(arr6);
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr4);
+// console.log(arr6);
 
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
