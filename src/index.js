5fee6fdff68311f119330054c8760d1bca862dc7
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-02 11:20:57 +0900

    初期化

--- src/index.js
+++ src/index.js
@@ -56,14 +56,14 @@
  * テンプレート文字列
  */
 
-const name = "じゃけえ";
-const age = 28;
-// 「私の名前はじゃけえです。年齢は28歳です」
+// const name = "じゃけえ";
+// const age = 28;
+// // 「私の名前はじゃけえです。年齢は28歳です」
 
-// 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です。"
-console.log(message1);
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。"
+// console.log(message1);
 
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
