616baa2f27a855278b7d2913035123ee9bc5b7e4
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-02 11:30:35 +0900

    初期化

--- src/index.js
+++ src/index.js
@@ -77,22 +77,22 @@
 //     return str;
 // }
 
-const func1 = function func1(str) {
-    return str;
-}
-console.log(func1("func1です"));
+// const func1 = function func1(str) {
+//     return str;
+// }
+// console.log(func1("func1です"));
 
-// アロー関数
-const func2 = (str) => {
-    return str
-}
-console.log(func2("func2です"));
+// // アロー関数
+// const func2 = (str) => {
+//     return str
+// }
+// console.log(func2("func2です"));
 
-const func3 = str => str
-console.log(func3("func3です"));
+// const func3 = str => str
+// console.log(func3("func3です"));
 
-const func4 = (num1, num2) => {
-    return num1 + num2;
-}
+// const func4 = (num1, num2) => {
+//     return num1 + num2;
+// }
 
-console.log(func4(10, 20));
+// console.log(func4(10, 20));
