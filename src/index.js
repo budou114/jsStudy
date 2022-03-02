5e92396ee20c7c877e4c04f294ce77556abe0fb1
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-02 11:39:53 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -96,3 +96,26 @@
 // }
 
 // console.log(func4(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//     name: "じゃけえ",
+//     age: 28
+// };
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const {name, age} = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+const myProfile = ['じゃけえ', 28];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+const[ name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
